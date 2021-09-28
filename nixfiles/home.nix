379e3caa1fe7fd2c8a849e9d3945bfe9889f5e80
Nebez Briefkani <me@nebezb.com>
2021-09-28 01:11:40 -0700

    Add instant prompt to p10k config

--- nixfiles/home.nix
+++ nixfiles/home.nix
@@ -29,6 +29,14 @@
       config = "git --git-dir=$HOME/.cfg/ --work-tree=$HOME";
       ll = "ls -al";
     };
+    initExtraFirst = ''
+      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+      # Initialization code that may require console input (password prompts, [y/n]
+      # confirmations, etc.) must go above this block; everything else may go below.
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+    '';
     initExtraBeforeCompInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
     initExtra = "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh";
     oh-my-zsh = {
