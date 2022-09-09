2fffe583909556bc3b519b26ff9e7d1a221d26f4
Nebez Briefkani <me@nebezb.com>
2022-09-09 12:03:30 -0700

    Refer to pkgs instead of nixos

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -69,7 +69,7 @@ in
           cat > shell.nix <<'EOF'
 let
     pkgs2205 = import (fetchTarball "http://nixos.org/channels/nixos-22.05/nixexprs.tar.xz") {};
-    pkgsUnstable = import (fetchTarball "http://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") {};
+    pkgsUnstable = import (fetchTarball "http://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz") {};
 in
     pkgs2205.mkShell {
         buildInputs = [
