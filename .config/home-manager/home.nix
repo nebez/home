d8c874cf1e0ebcad5e4a164cde7cae2fa527489a
Nebez Briefkani <me@nebezb.com>
2026-02-19 23:02:14 -0500

    lol wron config

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -57,6 +57,7 @@
       ssh-host-rm = "ssh-keygen -R";
       sm = "deno run --allow-all --no-check ~/code/github.com/nebez/sm/main.ts";
       home-manager-flake-update = "nix flake update --flake ~/.config/home-manager/";
+      cwdeploy = "deno run --allow-read --allow-run --allow-env jsr:@nebez/cwdeploy@0.2.0";
     };
     oh-my-zsh = {
       enable = true;
@@ -86,7 +87,6 @@
         aliases = "!git config -l | grep alias | cut -c 7-";
         l = "log --pretty=oneline --abbrev-commit";
         ll = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-        cwdeploy = "deno run --allow-read --allow-run --allow-env jsr:@nebez/cwdeploy@0.2.0";
       };
     };
     ignores = [
