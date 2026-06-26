1b2edc58a55b8c2a6de76dd21db129c67b2c7e5d
Nebez Briefkani <me@nebezb.com>
2026-06-25 21:28:07 -0400

    pin to 26.05 stable

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -2,9 +2,9 @@
   description = "Nebez home-manager config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
