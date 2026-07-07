3c804e556d226dedc4b1df5621573841a120f9e0
Nebez Briefkani <nebez@briefkani.com>
2026-07-06 23:15:44 -0400

    add brew and mise configs

--- .config/zsh/profile.zsh
+++ .config/zsh/profile.zsh
@@ -1,3 +1,4 @@
 export EDITOR="nano"
+export XDG_CONFIG_HOME="$HOME/.config"
 
 path=("$HOME/.local/bin" $path)
--- .config/zsh/rc.zsh
+++ .config/zsh/rc.zsh
@@ -1,1 +1,5 @@
+if command -v mise >/dev/null 2>&1; then
+  eval "$(mise activate zsh)"
+fi
+
 [[ -r "$HOME/.config/zsh/aliases.zsh" ]] && source "$HOME/.config/zsh/aliases.zsh"
